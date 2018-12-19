1a9093f7f636dd0841257fffc0601fe94212f06c
Juho Härme <juho.harme@gmail.com>
2018-12-19 14:32:18 +0200

    cleaning up

--- testenc.sh
+++
@@ -1,8 +0,0 @@
-fenc="$(file -bi  $1)"
-
-case "$fenc" in
-      *utf-8*) echo "String1 present" ;;
-      *utf-16*) echo "String2 present" ;;
-      *)         echo "else" ;;
-esac
-
--- tester2.sh
+++
@@ -1,4 +0,0 @@
-for var in "$@"
-do
-    echo "$var"
-done
